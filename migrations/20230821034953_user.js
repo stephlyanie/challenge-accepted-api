0bd1cfcc87c02413ceb6f2f82c5865e8186431ea
Stephanie Hall <stephanie.anie.hall@gmail.com>
2023-08-20 20:52:13 -0700

    Set up user migration file

--- migrations/20230821034953_user.js
+++ migrations/20230821034953_user.js
@@ -3,7 +3,17 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('user', (table) => {
+        table.uuid('id').primary();
+        table.string('username').notNullable();
+        table.string('first_name').notNullable();
+        table.string('last_name').nullable();
+        table.string('email').notNullable();
+        table.string('password').notNullable();
+        table.string('image_url').notNullable().defaultTo("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949");
+        table.boolean('verified').notNullable().defaultTo(0);
+        table.timestamps(true, true);
+      });
 };
 
 /**
@@ -11,5 +21,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('user');
 };
