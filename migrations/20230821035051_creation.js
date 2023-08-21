b6e138022bc5f22345de21ee1bce5de10c59bcd8
Stephanie Hall <stephanie.anie.hall@gmail.com>
2023-08-20 20:57:38 -0700

    Set up creation migration file

--- migrations/20230821035051_creation.js
+++ migrations/20230821035051_creation.js
@@ -3,7 +3,23 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('creation', (table) => {
+        table.uuid('id').primary();
+        table
+          .uuid('challenge_id')
+          .references('challenge.id')
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE');
+        table.string('name').notNullable();
+        table.string('description').notNullable();
+        table
+          .uuid('created_by_id')
+          .references('user.id')
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE');
+        table.string('image_url').notNullable().defaultTo("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1362px-Placeholder_view_vector.svg.png?20220519031949");
+        table.timestamps(true, true);
+      });
 };
 
 /**
@@ -11,5 +27,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('creation');
 };
