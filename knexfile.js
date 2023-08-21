dcc2bc4de4d03d1a72cc5254d27c964e581fda5d
Stephanie Hall <stephanie.anie.hall@gmail.com>
2023-08-20 20:49:20 -0700

    Set up knex file

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,14 @@
-// Update with your config settings.
+require('dotenv').config()
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
+    client: "mysql",
     connection: {
-      filename: './dev.sqlite3'
+        host: "127.0.0.1",
+        port: 3306,
+        user: process.env.DB_LOCAL_USER,
+        password: process.env.DB_LOCAL_PASSWORD,
+        database: process.env.DB_LOCAL_DBNAME,
+        charset: "utf8"
     }
-  },
+}; 
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
