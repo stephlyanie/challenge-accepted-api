f18cedd6ab4c98360d0bbaf9674bf09bd887d753
Stephanie Hall <stephanie.anie.hall@gmail.com>
2024-04-28 16:57:33 -0700

    Another test for configuration

--- knexfile.js
+++ knexfile.js
@@ -5,11 +5,11 @@ require('dotenv').config()
         client: "mysql",
         connection: {
             host: process.env.JAWSDB_URL,
-            port: 3306
-            // user: process.env.DB_USER,
-            // password: process.env.DB_PASSWORD,
-            // database: process.env.DBNAME,
-            // charset: "utf8"
+            port: 3306,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DBNAME,
+            charset: "utf8"
         }
     }
 // } else {
