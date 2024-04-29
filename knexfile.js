2eb615c8056103296a073e28dbc6a8464386c4ec
Stephanie Hall <stephanie.anie.hall@gmail.com>
2024-04-28 21:38:46 -0700

    Added pool to knexfile

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,12 @@ require('dotenv').config()
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
-            charset: "utf8"
+            charset: "utf8",
+            pool: {
+                max: 7,
+                min: 3,
+                acquireTimeout: 60 * 1000
+            }
         }
     }
 // } else {
