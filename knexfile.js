f800bdc76134833a71cf216035fda1f3ef72bc0f
Stephanie Hall <stephanie.anie.hall@gmail.com>
2024-07-24 11:45:00 -0700

    Updated process.env

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ require('dotenv').config()
         client: "mysql",
         connection: {
             host: process.env.DB_HOST,
-            // port: process.env.PORT,
+            port: process.env.PORT,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
