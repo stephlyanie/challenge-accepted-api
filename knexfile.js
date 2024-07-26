b31405e754d62c5c4fe6d74943b4aca193f4e0e1
Stephanie Hall <stephanie.anie.hall@gmail.com>
2024-07-25 17:43:46 -0700

    Setting up heroku

--- knexfile.js
+++ knexfile.js
@@ -1,26 +1,39 @@
 require('dotenv').config()
 
+const mysql = require('mysql');
+const connection ="";
+if(process.env.JAWSDB_URL) {  
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER_LOCAL,
+    password: process.env.DB_PASSWORD_LOCAL,
+    database: process.env.DBNAME_LOCAL,
+  });
+}
+
 // if (process.env.JAWSDB_URL) {
-    module.exports = {
-        client: "mysql",
-        connection: {
-            host: process.env.DB_HOST,
-            port: process.env.PORT,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            charset: "utf8",
-            // pool: {
-            //     max: 1,
-            //     min: 0,
-            //     createTimeoutMillis: 3000,
-            //     acquireTimeoutMillis: 10000,
-            //     idleTimeoutMillis: 30000,
-            //     reapIntervalMillis: 1000,
-            //     createRetryIntervalMillis: 100,
-            // }
-        }
-    }
+    // module.exports = {
+    //     client: "mysql",
+    //     connection: {
+    //         host: process.env.DB_HOST,
+    //         port: process.env.PORT,
+    //         user: process.env.DB_USER,
+    //         password: process.env.DB_PASSWORD,
+    //         database: process.env.DB_NAME,
+    //         charset: "utf8",
+    //         pool: {
+    //             max: 1,
+    //             min: 0,
+    //             createTimeoutMillis: 3000,
+    //             acquireTimeoutMillis: 10000,
+    //             idleTimeoutMillis: 30000,
+    //             reapIntervalMillis: 1000,
+    //             createRetryIntervalMillis: 100,
+    //         }
+    //     }
+    // }
 // } else {
 //     module.exports = {
 //         client: "mysql",
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use("/categories", categoriesRouter);
 const typesRouter = require("./routes/types");
 app.use("/types", typesRouter);
 
-app.listen(process.env.PORT || 8080, () =>
+app.connect(process.env.PORT || 8080, () =>
   console.log(`Server running`)
 );
 
