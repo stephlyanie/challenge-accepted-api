5ef240c037d249d47f3b90860d0704ef282aea12
Stephanie Hall <stephanie.anie.hall@gmail.com>
2024-07-31 16:24:11 -0700

    Connecting to Jaws DB

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,52 @@
 require('dotenv').config()
 
-const mysql = require('mysql');
-let connection;
-if(process.env.JAWSDB_PINK_URL) {  
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+const mysql = require('mysql2');
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DB_USER_LOCAL,
-    password: process.env.DB_PASSWORD_LOCAL,
-    database: process.env.DBNAME_LOCAL,
-  });
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
 }
+
+let connection;
+// if (process.env.JAWSDB_PINK_URL) {  
+//   module.exports = {
+//     client: "mysql",
+//     connection: mysql.createConnection(process.env.JAWSDB_PINK_URL),
+//     connectTimeout: 1000000,
+//     acquireTimeout: 1000000,
+//     waitForConnections: true,
+//     queueLimit: 0,
+//   }
+// } else {
+//   module.exports = {
+//     client: "mysql",
+//     connection: mysql.createConnection({
+//       host: 'localhost',
+//       user: process.env.DB_USER_LOCAL,
+//       password: process.env.DB_PASSWORD_LOCAL,
+//       database: process.env.DBNAME_LOCAL,
+//     }),
+//     pooling: {
+//       enabled: true,
+//       connectTimeout: 1000000,
+//       acquireTimeout: 1000000,
+//       waitForConnections: true,
+//       queueLimit: 0,
+//     }
+// }
+// }
 
 // if (process.env.JAWSDB_URL) {
     // module.exports = {
@@ -33,12 +68,13 @@ if(process.env.JAWSDB_PINK_URL) {
     //             createRetryIntervalMillis: 100,
     //         }
     //     }
-    // }
-// } else {
+    //   }
+// else {
 //     module.exports = {
 //         client: "mysql",
 //         connection: {
-//             host: "localhost",
+//             host:     // }
+// "localhost",
 //             user: process.env.DB_USER_LOCAL,
 //             password: process.env.DB_PASSWORD_LOCAL,
 //             database: process.env.DBNAME_LOCAL,
