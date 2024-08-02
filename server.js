0956404ef96656e64fc8b838f2f27ca404b8269a
Stephanie Hall <stephanie.anie.hall@gmail.com>
2024-08-02 13:05:13 -0700

    Defined bodyParser and timeout

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser')
+const timeout = require('connect-timeout')
 
 require("dotenv").config();
 
